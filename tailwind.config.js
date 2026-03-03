7189f3e1746e9146ae750b85b93565d3684eb1c4
Rabii <rabii.myself@gmail.com>
2026-03-03 14:42:02 +0000

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.js", "./src/**/*.js"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
